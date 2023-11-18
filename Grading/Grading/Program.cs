1d0924320228c1ff19b3cd7510459616075dfa16
Ibrahim Mohammed <36298932+Ib-Programmer@users.noreply.github.com>
2023-11-18 09:49:31 +0100

    Add files via upload
    student grading

---
+++ Grading/Grading/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the number of students: ");
+        int numberOfStudents = int.Parse(Console.ReadLine());
+
+        string[] studentNames = new string[numberOfStudents];
+        double[] studentScores = new double[numberOfStudents];
+        char[] studentGrades = new char[numberOfStudents];
+
+        // Input: Accepting names and scores
+        for (int i = 0; i < numberOfStudents; i++)
+        {
+            Console.Write($"Enter the name of student {i + 1}: ");
+            studentNames[i] = Console.ReadLine();
+
+            Console.Write($"Enter the score of {studentNames[i]}: ");
+            studentScores[i] = double.Parse(Console.ReadLine());
+        }
+
+        // Process: Grading
+        for (int i = 0; i < numberOfStudents; i++)
+        {
+            if (studentScores[i] >= 70)
+                studentGrades[i] = 'A';
+            else if (studentScores[i] >= 60)
+                studentGrades[i] = 'B';
+            else if (studentScores[i] >= 50)
+                studentGrades[i] = 'C';
+            else if (studentScores[i] >= 45)
+                studentGrades[i] = 'D';
+            else
+                studentGrades[i] = 'F';
+        }
+
+        // Output: Displaying names with grades
+        Console.WriteLine("\n\nStudent Grades:");
+        for (int i = 0; i < numberOfStudents; i++)
+        {
+            Console.WriteLine($"{studentNames[i]} - Grade: {studentGrades[i]}");
+        }
+    }
+}
+
